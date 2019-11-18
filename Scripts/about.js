0ba91282b226114330247fb25f26dbc1b5326cf3
Sujan Chegu <sujanchg@gmail.com>
2019-11-18 20:22:06 +0530

    Modified Home, Added About

---
+++ Scripts/about.js
@@ -0,0 +1,18 @@
+var option1 = document.getElementById("new");
+var option2 = document.getElementById("old");
+option1.addEventListener("click",newclick,false);
+option2.addEventListener("click",existclick,false);
+
+
+function newclick(e){
+    window.location.href = "#aboutus"
+}
+
+function existclick(e){
+    window.location.href = "#aboutus"
+}
+
+var uniname = document.getElementById("uniname");
+uniname.addEventListener("click",function() {
+    window.location.href = "home.html";
+},false)
